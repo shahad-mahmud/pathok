0ce501de10bb90fa6071631786d78c2966877966
shahad-mahmud <shahad9381@gmail.com>
2019-03-04 00:30:43 +0600

    change notification/status bar color in the splash screen near to the splash background

--- app/src/main/java/com/exodia/shahad/pathok/ActivitySplash.java
+++ app/src/main/java/com/exodia/shahad/pathok/ActivitySplash.java
@@ -2,6 +2,8 @@ package com.exodia.shahad.pathok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
 
 public class ActivitySplash extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class ActivitySplash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //status bar color
+        getWindow().setStatusBarColor(getResources().getColor(R.color.splash_back_notification));
     }
 }
