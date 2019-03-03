c0605652c62f75e90f78ad50a1fc0ef08c5f7152
shahad-mahmud <shahad9381@gmail.com>
2019-03-03 11:54:05 +0600

    splash activity modified, java class added, luncher activity changed

---
+++ app/src/main/java/com/exodia/shahad/pathok/ActivitySplash.java
@@ -0,0 +1,13 @@
+package com.exodia.shahad.pathok;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
